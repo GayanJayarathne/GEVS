7b9dc29d4fa63334cbed12c5d1e9daa2e6119692
Ravindu Abesinghe <93761338+Ravindu96Abesinghe@users.noreply.github.com>
2024-01-09 23:22:30 +0530

    partyDropDown

--- app/Http/Controllers/PartyController.php
+++ app/Http/Controllers/PartyController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use Faker\Guesser\Name;
 use Illuminate\Http\Request;
 
 class PartyController extends Controller
 {
-    //
+    public function partiesDropdown(Request $request)
+    {
+
+        $name = Name::get();
+
+        $obj = (object)array(
+            'name' => $name
+        );
+
+        return response()->json([
+            'result' => $obj,
+            'message' => 'ok',
+            'status' => 'success'
+        ]);
+    }
 }
