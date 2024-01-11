2f82e1456f16b93ff09a2e124a67cef9f691c880
Ravindu Abesinghe <93761338+Ravindu96Abesinghe@users.noreply.github.com>
2024-01-11 10:34:22 +0530

    registration

---
+++ app/Http/Controllers/Api/VoterController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class VoterController extends Controller
+{
+    public function index(Request $request)
+    {
+        return view('voter.index');
+    }
+//    public function __construct() {
+//        $this->middleware('auth');
+//    }
+////    public function index() {
+////        return view('voter.dashboard');
+////    }
+//
+//    public function store(Request $request) {
+//        if(isset($request['apply']))
+//        {
+//            $Nominee = new PositionUser();
+//            $Nominee->user_id = Auth::user()->id;
+//            $Nominee->position_id = $request['position_id'];
+//            // by default when voter apply for any position, its unapproved, until admin approves him/her
+//            $Nominee->Status = 0;
+//            $Nominee->votes = 0;
+//        }
+//        $user = User::findOrFail(Auth::user()->id);
+//        if($user->role == "voter"){
+//            $user->role = "candidate";
+//            $user->save();
+//        }
+//        $Nominee->save();
+//        $message = "Successfully applied as candidate";
+//        return redirect()->route('candidate.dashboard')->with('success',"Successfully applied as Candidate");
+//        //    return view('candidate.dashboard',compact('message'));
+//    }
+//
+//    public function apply() {
+//        $positions = Position::all();
+//        $departments = Department::all();
+//        return view('voter.apply',compact('departments','positions'));
+//    }
+}
