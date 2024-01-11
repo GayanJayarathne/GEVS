073cf21377c6e88c4c1e8bc1ed7b950b643e936b
Ravindu Abesinghe <93761338+Ravindu96Abesinghe@users.noreply.github.com>
2024-01-11 22:48:03 +0530

    rr

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -14,21 +14,22 @@ class HomeController extends Controller
     {
         $user = User::where('api_token',$request['api_token'])->first();
 
-        $allLead = Lead::where('user_id', $user->id);
-        
+//        $allLead = Lead::where('user_id', $user->id);
+        $allLead = Lead;
+
         $weeklyLeadCount  = (clone $allLead)->whereBetween('created_at', [Carbon::now()->startOfWeek(), Carbon::now()->endOfWeek()])->count();
         $monthlyLeadCount = (clone $allLead)->whereBetween('created_at', [Carbon::now()->startOfMonth(), Carbon::now()->endOfMonth()])->count();
         $totalLeadsCount  = (clone $allLead)->count();
         $recentLeads      = (clone $allLead)->orderby('created_at', 'desc')->limit(5)->get();
-        
+
         $data['totalLeads']   = $totalLeadsCount;
         $data['weeklyLeads']  = $weeklyLeadCount;
         $data['monthlyLeads'] = $monthlyLeadCount;
         $data['recentLeads']  = $recentLeads;
-        
+
         return response()->json([
             'message' => $data,
             'status'  => 'success'
-        ]); 
+        ]);
     }
 }
