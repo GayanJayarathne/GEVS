5feb12927cd5a77599eeab7b64c7890ef7baf6dd
GayanJayarathne <krgayan97@gmail.com>
2024-01-12 02:20:32 +0530

    xddd

--- resources/js/components/Dashboard.js
+++ resources/js/components/Dashboard.js
@@ -4,6 +4,7 @@ import { useSelector, connect } from 'react-redux';
 import rootAction from '../redux/actions/index'
 import { fadeIn } from 'animate.css'
 import 'iziToast/dist/css/iziToast.css';
+import {showSznNotification} from "../Helpers";
 
 const Dashboard = (props) => {
 
@@ -98,10 +99,47 @@ const Dashboard = (props) => {
     };
 
     const onVoteHandle = (id,name) => {
+
+        setState({
+            ...state,
+            loading: true
+        });
+
         axios.post('/api/v1/lead/updateVotes', {
             id:id,
             name:name,
             api_token: state.authUser.api_token,
+        }).then(response => {
+            setState({
+                ...state,
+                loading: false
+            });
+            if (response.data.status == 'validation-error') {
+                var errorArray = response.data.message;
+                $.each( errorArray, function( key, errors ) {
+                    $.each( errors, function( key, errorMessage ) {
+                        showSznNotification({
+                            type : 'error',
+                            message : errorMessage
+                        });
+                    });
+                });
+            } else if (response.data.status == 'error') {
+                showSznNotification({
+                    type : 'error',
+                    message : response.data.message
+                });
+            } else if (response.data.status == 'success') {
+                setState({
+                    ...state,
+                    voted: true
+                });
+                showSznNotification({
+                    type : 'success',
+                    message : response.data.message
+                });
+                history.push('/lead/list')
+            }
         }).catch((error) => {
             setState({
                 ...state,
@@ -129,8 +167,9 @@ const Dashboard = (props) => {
                                           aria-valuenow={lead.votes ? lead.votes : 0} aria-valuemin="0"
                                           aria-valuemax="100"></div>
                                 </div>:
-                                    <button type="button" className="btn btn-danger btn-sm btn-upper" onClick={() => onVoteHandle(lead?.id,lead?.name)}>Vote
-                                    </button>
+                                    (!state?.voted && <button type="button" className="btn btn-danger btn-sm btn-upper" onClick={() => onVoteHandle(lead?.id,lead?.name)}>Vote
+                            </button>)
+
                                 }
                             </td>
                         </tr>;
