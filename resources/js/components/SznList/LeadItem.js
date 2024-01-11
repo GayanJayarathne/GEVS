5c68c94f2bc20ede3dc852a061bdc46fa5fb027d
GayanJayarathne <krgayan97@gmail.com>
2024-01-11 22:50:13 +0530

    ggg

--- resources/js/components/SznList/LeadItem.js
+++ resources/js/components/SznList/LeadItem.js
@@ -2,115 +2,128 @@ import React, { Component } from 'react'
 import moment from 'moment';
 import { Link } from 'react-router-dom';
 
-class LeadItem extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
+const LeadItem =(props)=> {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //
+    //     }
+    //
+    // }
+    //
+    // componentDidMount() {
+    //
+    // }
+
+    const renderConstituency = (Id) => {
+        switch (Id) {
+            case "1":
+                return "Shangri-la-Town";
+            case "2":
+                return "Northern-Kunlun-Mountain";
+            case "3":
+                return "Western-Shangri-la";
+            case "4":
+                return "Naboo-Vallery";
+            case "5":
+                return "New-Felucia";
 
         }
-
     }
 
-    componentDidMount() {
-        
+    const renderParty = (Id) => {
+        switch (Id) {
+            case "1":
+                return "Blue Party";
+            case "2":
+                return "Red Party";
+            case "3":
+                return "Yellow Party";
+            case "4":
+                return "Independent";
+
+        }
     }
 
-    render() {
+    const renderPartyColor = (Id) => {
+        switch (Id) {
+            case "1":
+                return "Blue";
+            case "2":
+                return "Red";
+            case "3":
+                return "Yellow";
+            case "4":
+                return "Green";
+
+        }
+    }
+
+    // render() {
         return (
             <React.Fragment>
                 <div className="szn-portlet">
                     <div className="szn-portlet__body">
                         <div className="szn-widget szn-widget--user-profile-3">
                             <div className="szn-widget__top">
-                                { true ? 
+                                { true ?
                                 <div className="szn-widget__media szn-hidden-">
                                     <img src="/assets/images/faces/face1.jpg" alt="image" />
                                 </div> :
                                 <div
                                     className="szn-widget__pic szn-widget__pic--danger szn-font-danger szn-font-boldest szn-font-light ">
-                                    {this.props.obj.name.split(' ').map(function(str) { return str ? str[0].toUpperCase() : "";}).join('')}
+                                    {props.obj.name.split(' ').map(function(str) { return str ? str[0].toUpperCase() : "";}).join('')}
                                 </div> }
                                 <div className="szn-widget__content">
                                     <div className="szn-widget__head">
                                         <Link to={{
-                                            pathname: `/lead/edit/${this.props.obj.id}`,
+                                            pathname: `/lead/edit/${props.obj.id}`,
                                             state: {
-                                                lead: this.props.obj
+                                                lead: props.obj
                                             }
                                         }} className="szn-widget__username">
-                                            {this.props.obj.name}
-                                            { this.props.obj.status == 0 ? <i className="mdi mdi-close-circle-outline szn-font-danger"></i> 
+                                            {props.obj.name}
+                                            { props.obj.status == 0 ? <i className="mdi mdi-close-circle-outline szn-font-danger"></i>
                                             : <i className="mdi mdi-checkbox-marked-circle szn-font-success"></i> }
                                         </Link>
                                         <div className="szn-widget__action">
                                             <Link to={{
-                                                pathname: `/lead/edit/${this.props.obj.id}`,
+                                                pathname: `/lead/edit/${props.obj.id}`,
                                                 state: {
-                                                    lead: this.props.obj
+                                                    lead: props.obj
                                                 }
                                             }} type="button" className="btn btn-outline-success btn-sm btn-upper">Edit</Link>&nbsp;
-                                            <button type="button" className="btn btn-danger btn-sm btn-upper" onClick={() => this.props.onClickDeleteHandler(this.props.obj.id)}>Delete</button>
+                                            {/*<button type="button" className="btn btn-danger btn-sm btn-upper" onClick={() => props.onClickDeleteHandler(props.obj.id)}>Delete</button>*/}
                                         </div>
                                     </div>
                                     <div className="szn-widget__subhead d-flex flex-column flex-md-row">
-                                        <a href={void(0)}><i className="mdi mdi-email"></i>{this.props.obj.email}</a>
-                                        <a href={void(0)}><i className="mdi mdi-phone"></i>{this.props.obj.phone} </a>
-                                        { this.props.obj.address ? <a href={void(0)}><i className="mdi mdi-home"></i>{this.props.obj.address}</a> : '' }
+                                        <a href={void(0)}><i className="mdi mdi-map-marker"></i>{renderConstituency(props.obj.constituency_id)}</a>
+                                        <a href={void(0)}><i className="mdi mdi-flag" style={{color:renderPartyColor(props.obj.party_id)}}></i>{renderParty(props.obj.party_id)} </a>
                                     </div>
-                                    <div className="szn-widget__info">
-                                        <div className="szn-widget__desc">
-                                            {this.props.obj.description} 
-                                        </div>
-                                        <div className="szn-widget__progress">
-                                            <div className="szn-widget__text">
-                                                Progress
-                                            </div>
-                                            <div className="progress" style={{ height: '5px', width: '100%' }}>
-                                                <div className="progress-bar szn-bg-success" role="progressbar" style={{ width: this.props.obj.progress+'%' }}
-                                                    aria-valuenow={this.props.obj.progress} aria-valuemin="0" aria-valuemax="100"></div>
-                                            </div>
-                                            <div className="szn-widget__stats">
-                                                {this.props.obj.progress}%
-                                            </div>
-                                        </div>
-                                    </div>
+                                    {/*<div className="szn-widget__info">*/}
+                                    {/*    <div className="szn-widget__progress">*/}
+                                    {/*        <div className="szn-widget__text">*/}
+                                    {/*            Progress*/}
+                                    {/*        </div>*/}
+                                    {/*        <div className="progress" style={{ height: '5px', width: '100%' }}>*/}
+                                    {/*            <div className="progress-bar szn-bg-success" role="progressbar" style={{ width: props.obj.progress+'%' }}*/}
+                                    {/*                aria-valuenow={props.obj.progress} aria-valuemin="0" aria-valuemax="100"></div>*/}
+                                    {/*        </div>*/}
+                                    {/*        <div className="szn-widget__stats">*/}
+                                    {/*            {props.obj.progress}%*/}
+                                    {/*        </div>*/}
+                                    {/*    </div>*/}
+                                    {/*</div>*/}
                                 </div>
                             </div>
                             <div className="szn-widget__bottom d-flex flex-column flex-md-row">
                                 <div className="szn-widget__item ">
                                     <div className="szn-widget__icon">
-                                        <i className="mdi mdi-currency-usd"></i>
+                                        <i className="mdi mdi-close-box"></i>
                                     </div>
                                     <div className="szn-widget__details d-flex flex-md-column flex-row">
-                                        <span className="szn-widget__title">Earnings</span>
-                                        <span className="szn-widget__value"><span>$</span>{this.props.obj.earnings}</span>
-                                    </div>
-                                </div>
-                                <div className="szn-widget__item">
-                                    <div className="szn-widget__icon">
-                                        <i className="mdi mdi-cart-outline"></i>
-                                    </div>
-                                    <div className="szn-widget__details d-flex flex-md-column flex-row">
-                                        <span className="szn-widget__title">Expenses</span>
-                                        <span className="szn-widget__value"><span>$</span>{this.props.obj.expenses}</span>
-                                    </div>
-                                </div>
-                                <div className="szn-widget__item">
-                                    <div className="szn-widget__icon">
-                                        <i className="mdi mdi-chart-arc"></i>
-                                    </div>
-                                    <div className="szn-widget__details d-flex flex-md-column flex-row">
-                                        <span className="szn-widget__title">Net</span>
-                                        <span className="szn-widget__value"><span>$</span>{this.props.obj.net}</span>
-                                    </div>
-                                </div>
-                                <div className="szn-widget__item">
-                                    <div className="szn-widget__icon">
-                                        <i className="mdi mdi-calendar"></i>
-                                    </div>
-                                    <div className="szn-widget__details d-flex flex-md-column flex-row">
-                                        <span className="szn-widget__title">Added On</span>
-                                        <span className="szn-widget__value">{moment(this.props.obj.created_at).fromNow()}</span>
+                                        <span className="szn-widget__title">Votes</span>
+                                        <span className="szn-widget__value">{props.obj.votes}</span>
                                     </div>
                                 </div>
                             </div>
@@ -119,7 +132,7 @@ class LeadItem extends Component {
                 </div>
             </React.Fragment>
         )
-    }
+    // }
 }
 
 export default LeadItem
