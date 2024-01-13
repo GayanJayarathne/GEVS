4c646991b5ea4a7217f6d57e27110c7971cbc9d6
Ravindu Abesinghe <93761338+Ravindu96Abesinghe@users.noreply.github.com>
2024-01-13 20:55:42 +0530

    rr

---
+++ app/Http/Controllers/Api/VotingStartController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\Lead;
+use App\Http\Controllers\User;
+use App\Http\Controllers\Validator;
+use App\Models\VotingStart;
+use Illuminate\Http\Request;
+
+class VotingStartController extends Controller
+{
+    public function listData(Request $request)
+    {
+
+        $voting_start = VotingStart::get();
+
+        return response()->json([
+            'message' => $voting_start,
+            'status' => 'success'
+        ]);
+    }
+
+
+    public function create(Request $request)
+    {
+        $user = User::where('api_token',$request['api_token'])->first();
+
+        $validate = Validator::make($request->all(), [
+            'date'        => 'required|date',
+            'start_time'  => 'time',
+            'end_time'    => 'time'
+        ]);
+
+        if ($validate->fails()) {
+            return response()->json([
+                'message' => $validate->errors(),
+                'status' => 'validation-error'
+            ], 401);
+        }
+
+        $newVotingStart = Lead::create([
+            'date'        => $request['date'],
+            'start_time'  => $request['start_time'],
+            'end_time'    => $request['end_time']
+        ]);
+
+        if ($newVotingStart) {
+            return response()->json([
+                'message' => 'Voting Start successfully saved',
+                'status' => 'success'
+            ]);
+        } else {
+            return response()->json([
+                'message' => 'Something went wrong',
+                'status' => 'error'
+            ]);
+        }
+    }
+}
